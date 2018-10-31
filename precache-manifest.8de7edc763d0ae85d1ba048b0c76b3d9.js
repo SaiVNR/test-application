8eade6a568bccae41d776468d122f8fa7e0b1ad4
Saiteja Suggula <Saiteja_Suggula@epam.com>
2018-10-31 10:08:05 +0530

    Updates

---
+++ precache-manifest.8de7edc763d0ae85d1ba048b0c76b3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-application/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5b4d1b4e24ba8d85e70e",
+    "url": "/test-application/static/js/runtime~main.5b4d1b4e.js"
+  },
+  {
+    "revision": "a204ee981e0f857f13c0",
+    "url": "/test-application/static/js/main.a204ee98.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/test-application/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "a204ee981e0f857f13c0",
+    "url": "/test-application/static/css/main.58dc8372.chunk.css"
+  },
+  {
+    "revision": "c5a20ef8f076bc9ca5a0a0f3c9aa294b",
+    "url": "/test-application/index.html"
+  }
+];
